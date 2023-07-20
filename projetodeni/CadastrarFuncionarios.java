b41da90fec82faa292188c13e9ecb72407f84369
Everton <76123738+Evert0nLuiz@users.noreply.github.com>
2023-07-20 08:13:26 -0300

    Add files via upload

---
+++ projetodeni/CadastrarFuncionarios.java
@@ -0,0 +1,18 @@
+package org.evertonprojetos.projetodeni;
+import org.evertonprojetos.projetodeni.telas.TelaPrincipal;
+
+public class CadastrarFuncionarios {
+
+    public static void main(String[] args) {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
+        telaPrincipal.setBounds(400,140,700,500);
+        telaPrincipal.setContentPane(telaPrincipal.painelPrincipal);
+
+
+
+
+
+
+    }
+}
